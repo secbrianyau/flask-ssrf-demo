82d3279bf50a0d50062a0a01d3f0faa28e891a32
secbrianyau <90521014+secbrianyau@users.noreply.github.com>
2021-12-11 17:01:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/website
 export FLASK_ENV=development
-export FLASK_APP=/home/ubuntu/website/main.py
+export FLASK_APP=./main.py
 nohup python3 -m flask run --host=0.0.0.0 --port=80 > web.log &
